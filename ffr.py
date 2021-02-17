ac0f113a38e2cf10ba85959088a617fe4a4e1dcc
jhaprashant079 <jhaprashant079@gmail.com>
2021-02-17 13:21:21 +0530

    first code

--- ffr.py
+++ ffr.py
@@ -1,2 +1,24 @@
-print('kuchh bhi!!!!!!!!!')
-print(6)
+import subprocess as sp
+location=input("Enter file location here : ")
+#general tools
+#binwalk
+sp.run('binwalk -e {}'.format(location),shell=True)
+wd=sp.run('pwd',shell=True,capture_output=True,text=True)
+ploc=re.compile(r'/')
+ext_location=wd+"/_" + ploc.split(location)[-1] + ".extracted"
+
+
+
+#file identification using file command
+def file_identifier(location):
+    filecmd=sp.run('file {}'.format(location),shell=True,capture_output=True,text=True)
+    return filecmd
+pjpg=re.compile(r'jpg|.jpg')
+ppng=re.compile(r'png|.png')
+
+
+
+if len(pjpg.findall(filecmd)):
+    import jpg.py
+if len(ppng.findall(filecmd)):
+    import png.py
