3ee38d4ee8019fdfb3b6c99df92aee1744377bfe
jhaprashant079 <jhaprashant079@gmail.com>
2021-04-10 21:55:43 +0530

    Text file analysis

--- txt.py
+++ txt.py
@@ -1,3 +1,7 @@
-from generalTools import hexdump
+import subprocess as sp
+R=open("/home/jhaprashant079/project/README.md","a")
 def data_from_txt(loc):
-    hexdump(loc)
+    #stegsnow
+    a=sp.Popen("stegsnow -C {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    R.write(a+"\n")
+    R.close()
