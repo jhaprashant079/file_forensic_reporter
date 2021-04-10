346d84f6aa4634fca957d3e040e7ecd129d27e1d
jhaprashant079 <jhaprashant079@gmail.com>
2021-04-10 21:51:12 +0530

    general tools for all file types

--- generalTools.py
+++ generalTools.py
@@ -1,6 +1,8 @@
 import subprocess as sp
 import re
+import hashlib
 #general tools
+R=open("Report.md","a")
 def f_name(location) :
     if '/' in location:
         ploc=re.compile(r'/')
@@ -15,40 +17,56 @@ def f_type(location):
     return l[1:]
 def genToolz(location):
     #exiftool
-    print("Exifdata : ")
-    exifdata=sp.run('exiftool {}'.format(location),shell=True,capture_output=True,text=True)
-    #..
+    a=sp.Popen('exiftool {}'.format(location),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    exifdata=re.sub(r'(File Permissions.*\n)|(ExifTool Version Number.*\n)','',a)
+    R.write("Exifdata======================\n"+exifdata)
+    
+    #hexdump & string
+    b=sp.Popen(r'xxd {} | head'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0]
+    c=sp.Popen(r'xxd {} | tail'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0]
+    d=sp.Popen(r'strings {} | tail'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0] 
+    R.write("Head of hexdump:\n"+b+"\nTail of hexdump:\n"+c+"\nLast lines of Readable characters from file:\n"+d+"\n")
     
     #binwalk
-    binchoice=input('Do you wanna use binwalk on this file,(y/n)')
-    if binchoice=='y' :
-        sp.run('binwalk {}'.format(location),shell=True)
-        savebin=input("Do you wanna extract the files (y/n):")
-        if savebin=='y':
-            wd=sp.run('pwd',shell=True,capture_output=True,text=True)
+    try:
+        e=sp.Popen('binwalk {}'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0]
+        f=re.sub(r'(-{80}\n)|(.*Zlib.*)|(^0\s.*\n)','',e,re.MULTILINE)
+        if len(re.findall(r'.+',f))>0:
+            l=sp.Popen('binwalk -e {}'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0] 
+            wd=sp.Popen('pwd',shell=True,stdout=sp.PIPE,text=True).communicate()[0]
             ext_location=wd+"/_" + f_name(location) + ".extracted"
-            print('Files and folders in binwalk extracted folder :')
-            sp.run('tree -la {}/'.format(ext_location))
-    #foremost
-    forechoice=input('Do you wanna use foremost on this file,(y/n) :')
-    if forechoice=='y' :
-        wd=sp.run('pwd',shell=True,capture_output=True,text=True)
-        sp.run('mkdir recovery_{}'.format(f_name(location)))
-        sp.run('foremost -i {} -T -o recovery_{}'.format(location,f_name(location)),shell=True,c)
-        print("Recovered files are saved in recovery_{} directory.".format(f_name))
-#hexdump & string
-def hexdump(location):
-    hexchoice=input("Do you wanna see hexdump of the file (y/n):")
-    strchoice=input("Do you wanna see strings of the file (y/n),if u r searching for particular word type that after space with y:")
-    svchoice=input('Do you wanna save it in a file (y/n):')
-    if hexchoice=='y' :
-        print("Hexdump of the file: ")
-        sp.run(r'hexdump -C {}'.format(loc),shell=True,text=True)
-        if strchoice=='y':
-            s=sp.Popen('string {} | grep -i {}'.format(location,strchoice[2:]),stdout=sp.PIPE,shell=True,text=true).communicate()[0]
-        if svchoice=='y' :
-            print(r'The hexdump is saved in file "{}_hexdump"'.format(loc))
-            sp.run(r'hexdump -C {} > {}_hexdump',format(loc,loc),shell=True)
-        sp.run(r'strings {}'.format(loc),shell=True)  
-    if hexchoice!='y' or hexchoice!='n' :
-        print("Please give valid response!")
+            R.write('Binwalk extracted result in '+ ext_location+'/n')
+        #foremost
+        g=sp.Popen(r'foremost {}'.format(location),shell=True,text=True,stdout=sp.PIPE).communicate()[0]
+        if len(re.findall(r'^.*\n\|\*\|',g,re.MULTILINE))==0 :
+            wd=sp.run('pwd',shell=True,capture_output=True,text=True)
+            sp.run('mkdir recovery_{}'.format(f_name(location)),shell=True)
+            sp.run('foremost -i {} -T -o recovery_{}'.format(location,f_name(location)),shell=True)
+            R.write("Recovered files are saved in recovery_{} directory.".format(f_name))
+    except:
+        R.write('binwalk and foremost failed.')
+def md5hash(filename):
+    h = hashlib.md5()
+    with open(filename,'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
+    return h.hexdigest()
+def sha1hash(filename):
+    h = hashlib.sha1()
+    with open(filename,'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
+    return h.hexdigest()
+def sha256hash(filename):
+    h = hashlib.sha256()
+    with open(filename,'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
+    return h.hexdigest()
+    R.close()
