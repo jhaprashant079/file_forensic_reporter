40cc3fd0c8d4f66049ae9e4fdd54c4e314394b6b
jhaprashant079 <jhaprashant079@gmail.com>
2021-04-10 21:55:13 +0530

    Office file type analysis

--- office_files.py
+++ office_files.py
@@ -4,45 +4,36 @@ from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XM
 import oletools.oleid
 
 
-R=open("/home/jhaprashant079/project/README.md","a")
-def data_from_doc_or_xls(loc):
+R=open("Report.md","a")
+def data_from_officeFile(loc):
     #mraptor
-    s=sp.Popen("mraptor {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate[0]
-    R.write(s)
-    #olemeta
-    a=sp.Popen("olemeta {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate[0]
-    R.write(a)
-    #oletime
-    b=sp.Popen("oletime {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate[0]
-    R.write(b)
+    s=sp.Popen("mraptor {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    s=re.sub("^(.*\n.*\n)",'',s)
+    R.write("\nData from mraptor:\n:"+s+"\n")
     #olevba
-    #parsing office files
-    filedata = open(myfile, 'rb').read()
-    vbaparser = VBA_Parser(loc, data=filedata)
-    if vbaparser.detect_vba_macros():#Detect VBA macros
-        #extracting vba macro source code
-        for (filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
-            R.write('-'*79 + '\n')
-            R.write('Filename    :' + filename + '\n')
-            R.write('OLE stream  :' + stream_path + '\n')
-            R.write('VBA filename:' + vba_filename + '\n')
-            R.write('- '*39 + '\n')
-            R.write(vba_code + '\n')
-        #analyze source code
-        results = vbaparser.analyze_macros()
-        for kw_type, keyword, description in results:
-            R.write('type=%s - keyword=%s - description=%s' % (kw_type, keyword, description))
-        #Deobfuscate VBA Macro Source Code
-        R.write("Deobfuscated macro source code : \n")
-        R.write(vbaparser.reveal())
+    a=sp.Popen("olevba --reveal --decode {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    a=re.sub("^(.*\n)",'',a)
+    R.write("Data from olevba:\n"+a+"\n")
     #oleid
-    oid = oletools.oleid.OleID(loc)
-    indicators = oid.check()
-    for i in indicators:
-        R.write("#OLEID:")
-        R.write('Indicator id=%s name="%s" type=%s value=%s' % (i.id, i.name, i.type, repr(i.value)) + "\n")
-        R.write('description:' + i.description + "\n")
+    a=sp.Popen("oleid {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    a=re.sub("^(.*\n.*\n.*\n)",'',a)
+    z=re.findall('OLE format.*',a,re.MULTILINE)[0]
+    R.write("Data from oleid:\n"+a+"\n")
+    #msodde
+    a=sp.Popen("msodde -a {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    a=re.sub("^(.*\n.*\n.*\n)",'',a)
+    R.write("Data from msodde:\n"+a+"\n")
     #detecting and extracting a SWF file from a Word document
-    s=sp.Popen("pyxswf -o {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate[0]
-    R.write(s+'\n')
-    #oledump.py worddoc.doc
+    if "False" not in z:
+        s=sp.Popen("pyxswf -o {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+        R.write("Data from pyxwf:\n"+s+'\n')
+    #oleobj
+    a=sp.Popen("oleobj {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+    a=re.sub("^(.*\n.*\n.*\n)",'',a)
+    R.write("Data from olevbj:\n"+a+"\n")
+    #olemeta
+    if "False" not in z:
+        a=sp.Popen("olemeta {}".format(loc),shell=True,stdout=sp.PIPE,text=True).communicate()[0]
+        a=re.sub("^(.*/n.*/n)",'',a)
+        R.write("Data from olemeta:\n"+a+"\n")
+    R.close()
